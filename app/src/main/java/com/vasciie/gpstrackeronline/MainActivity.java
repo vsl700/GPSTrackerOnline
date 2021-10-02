ce8d5fe344ebdf96f31ed8f399a41f1fbc7d0093
vsl700 <51147745+vsl700@users.noreply.github.com>
2021-10-02 21:51:41 +0300

    Initial commit

---
+++ app/src/main/java/com/vasciie/gpstrackeronline/MainActivity.java
@@ -0,0 +1,46 @@
+package com.vasciie.gpstrackeronline;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentOnAttachListener;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap gMap;
+    private OnMapReadyCallback thisActivity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .setReorderingAllowed(true)
+                    .add(R.id.primary_map, SupportMapFragment.class, null)
+                    .commit();
+        }
+
+        thisActivity = this;
+        getSupportFragmentManager().addFragmentOnAttachListener(new FragmentOnAttachListener() {
+            @Override
+            public void onAttachFragment(@NonNull FragmentManager fragmentManager, @NonNull Fragment fragment) {
+                ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.primary_map))
+                        .getMapAsync(thisActivity);
+            }
+        });
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        gMap = googleMap;
+    }
+}
