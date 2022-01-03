0e02e8218ae48df7416fd608ada601c5b7f01bd6
vsl700 <51147745+vsl700@users.noreply.github.com>
2022-01-03 20:40:50 +0200

    Just getting started with implementing SignalR (and InternetService)

---
+++ app/src/main/java/com/vasciie/gpstrackeronline/services/InternetService.java
@@ -0,0 +1,17 @@
+package com.vasciie.gpstrackeronline.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.IBinder;
+
+
+
+public class InternetService extends Service {
+
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
